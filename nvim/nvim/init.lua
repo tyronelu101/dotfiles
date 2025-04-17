bcd81ef513dedeaa5dd0749c6531f2e1bfe0678c
Tyrone <tyrone.lu01@gmail.com>
2025-04-16 21:34:43 -0500

    initial commit

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,13 @@
+--Enables absolute line numbers
+vim.wo.number = true
+--Enables relative number(combined with absolute, current line will be absolute while rest is relative)
+vim.wo.relativenumber = true
+
+--How many spaces a tab is \t when displayed
+vim.opt.tabstop = 2
+--How many spaces when auto-indenting
+vim.opt.shiftwidth = 2
+--Converts all tabs to spaces
+vim.opt.expandtab = true
+--How many spaces pressing Tab or Backspace while in insert mode
+vim.bo.softtabstop = 2
---
+++ zsh/.zshrc
@@ -0,0 +1,16 @@
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+#History
+setopt histignorealldups sharehistory
+HISTFILE=$HOME/.zhistory
+SAVEHIST=1000
+HISTSIZE=1000
+autoload -U compinit && compinit
+
+#Highlighting
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+#Autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+#Sets up zsh shell to use Starship
+eval "$(starship init zsh)"
