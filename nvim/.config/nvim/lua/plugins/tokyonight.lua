88168253a828e5e36dfbcca184f5b4213c725beb
Tyrone <tyrone.lu01@gmail.com>
2025-04-17 21:26:40 -0500

    apply tokyonight theme inside the plugin instead of init.lua

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,7 +1,11 @@
 return {
   "folke/tokyonight.nvim",
+  lazy = false,
   opts = {
     style = "moon", -- options: "storm", "moon", "night", "day"
     transparent = false,
   },
+  config = function()
+    vim.cmd([[colorscheme tokyonight]])
+  end,
 }
