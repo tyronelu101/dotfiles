e6a6a744ba5e1f01c07a0dd15410d92ac50c177d
Tyrone <tyrone.lu01@gmail.com>
2025-04-17 21:53:43 -0500

    add treesitter plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,8 @@ vim.wo.number = true
 --Enables relative number(combined with absolute, current line will be absolute while rest is relative)
 vim.wo.relativenumber = true
 
+--Highlighting cursor line
+vim.opt.cursorline = true
 --How many spaces a tab is \t when displayed
 vim.opt.tabstop = 2
 --How many spaces when auto-indenting
@@ -13,6 +15,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 --How many spaces pressing Tab or Backspace while in insert mode
 vim.bo.softtabstop = 2
-
+--Allow terminal application colors
+vim.opt.termguicolors = true
 
 vim.cmd.colorscheme("tokyonight")
