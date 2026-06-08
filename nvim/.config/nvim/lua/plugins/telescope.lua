71a95744cd4cb1c394279ca307d200358b4f4305
Tyrone <tyronelu99@gmail.com>
2026-06-08 10:05:01 -0500

    add keybindings for telescope.lua

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,13 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",  desc = "Find Files (Root Dir)" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find Files (Grep)},
+      { "<leader>fb", "<cmd>Telescope Telescope buffers", desc = "Find Files (Buffer)}
+    },
+} 
