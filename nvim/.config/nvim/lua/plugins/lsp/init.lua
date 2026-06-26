88aadf1a78810972008cacb080edbd2fc7c73cff
ty <tyrone.lu01@gmail.com>
2026-06-25 23:31:46 -0500

    start setting up lsp

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,18 +1,10 @@
 return {
-  -- lsp config
-  { 
+  {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      { "mason-org/mason.nvim", config = function() end }, 
+      "mason-org/mason.nvim",
+      { "mason-org/mason-lspconfig.nvim", config = function() end },
     },
-    opts = {
-      servers = {
-        rust_analyzer = { enabled = false },
-      },
-    },
-    config = function()
-      require("mason").setup()
-      require("lspconfig").lua_ls.setup{}
-    end
-  }
+  },
 }
