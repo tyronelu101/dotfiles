5036202398a21061d6a7780d4a9b349478c8cb88
Tyrone <tyronelu99@gmail.com>
2026-06-27 14:11:21 -0500

    enable lsp

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -2,6 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
+    enabled = true,
     dependencies = {
       "mason-org/mason.nvim",
       { "mason-org/mason-lspconfig.nvim", config = function() end },
