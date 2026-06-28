ad88cc2a222848ccb9bdd7ff9f50bb11741295ba
ty <tyrone.lu01@gmail.com>
2026-06-28 11:57:53 -0500

    basic setup of python and rust lsp

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,10 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    -- event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      "mason-org/mason.nvim",
-      { "mason-org/mason-lspconfig.nvim", config = function() end },
+      { "mason-org/mason.nvim", opts = {}},
+      { "mason-org/mason-lspconfig.nvim", 
+      	opts = {
+	ensure_installed = {"pyright", "rust_analyzer"}
+      	}
+      },
     },
+    
   },
 }
